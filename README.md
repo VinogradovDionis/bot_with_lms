17255f89367408e9656f5dd092fad9cabcf96b21
VinogradovDionis <elova.moore@yandex.ru>
2021-03-02 23:43:40 +0300

    The bot

---
+++ README.md
@@ -0,0 +1,6 @@
+![preview](prev.gif)
+
+
+© Vinogradov Ruslan, School in Kapotny
+
+Licensed under the [MIT License](LICENSE).
---
+++ bot.py
@@ -0,0 +1,31 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from config import tocken
+from handlers import help_1,start,work,black_work,inl_but,help_2,one,two
+
+
+
+# функция main запускает бота 	
+
+def main():
+	bot = Updater(tocken)
+	bot.start_polling()
+	bot.dispatcher.add_handler(CommandHandler('start',start))
+	
+	bot.dispatcher.add_handler(MessageHandler(Filters.regex('Теория'),work))
+	bot.dispatcher.add_handler(MessageHandler(Filters.regex('В главное меню'),start))
+	bot.dispatcher.add_handler(MessageHandler(Filters.regex('Практика'),black_work))	
+	bot.dispatcher.add_handler(CallbackQueryHandler(inl_but))
+
+	bot.dispatcher.add_handler(CommandHandler('help',help_1))
+	bot.dispatcher.add_handler(MessageHandler(Filters.regex('/help'),help_2))
+
+	bot.dispatcher.add_handler(MessageHandler(Filters.regex('/1'),one))
+	bot.dispatcher.add_handler(MessageHandler(Filters.regex('/2'),two))
+
+	
+	bot.idle()
+
+# стандартный запуск функции main
+
+if __name__ == '__main__':
+	main()
